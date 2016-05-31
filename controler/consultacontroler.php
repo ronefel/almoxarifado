174e3a143eba082a705e7a6a4a2ce2f2d6c48fd5
Rone Santos <ronefel@hotmail.com>
2016-05-31 01:36:12 -0400

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- controler/consultacontroler.php
+++ controler/consultacontroler.php
@@ -20,26 +20,35 @@ if (isset($_POST['patrimonioid'])) {
 }
 
 switch ($control) {
-    
+
     case 'consulta': {
+
+            if (strlen($patrimonio->getPatrimonioid()) > 0) {
+
+                $patrimonio = patrimonioAction::getPatrimonio($patrimonio);
 
                 if (strlen($patrimonio->getPatrimonioid()) > 0) {
 
-                    $patrimonio = patrimonioAction::getPatrimonio($patrimonio);
-                    
                     $JSON = "{\"Patrimônio\":\"{$patrimonio->getPatrimonioid()}\","
                             . "\"Produto\":\"{$patrimonio->getProdutonome()}\","
                             . "\"Local\":\"{$patrimonio->getLocalnome()}\","
                             . "\"Departamento\":\"{$patrimonio->getDepartamentonome()}\","
-                            . "\"Estado de Conservação\":\"{$patrimonio->getEstadoconservacao()}\"}";
-                    
-                    $msg[]= $JSON;
-                } else {
+                            . "\"Data da Compra\":\"{$patrimonio->getDatacompra(TRUE)}\","
+                            . "\"Data Implantação\":\"{$patrimonio->getDataimplantacao(TRUE)}\","
+                            . "\"Fim da Garantia\":\"{$patrimonio->getFimgarantia(TRUE)}\","
+                            . "\"Estado de Conservação\":\"{$patrimonio->getEstadoconservacao()}\","
+                            . "\"Fornecedor\":\"{$patrimonio->getFantazia()}\","
+                            . "\"Observação\":\"{$patrimonio->getObs()}\"}";
 
-                    $msg[] = "O campo 'patrimônio' não pode ser vazio!";
+                    $msg[] = $JSON;
+                } else {
+                    $msg[] = "Patrimônio não encontrado!";
                 }
+            } else {
+
+                $msg[] = "Por favor digite um número válido!";
+            }
         }break;
-    
 }
 
 if (count($msg) > 0) {
--- patrimonio.php
+++ patrimonio.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'config.php';
+include_once './script.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,20 +12,28 @@ include_once 'config.php';
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <script src="js/jquery-1.11.2.min.js"></script>
         <script src="themes/redmond/jquery-ui.min.js"></script>
+        <script src="js/jquery.dataTables.min2.js"></script>
         <link rel="stylesheet" href="css/css.css">        
         <link rel="stylesheet" href="themes/redmond/jquery-ui.min.css">
+        <link rel="stylesheet" href="css/jquery.dataTables_themeroller.css">
         <script>
             $(function () {
+
+                var tips = $(".msg");
+                function updateTips(t) {
+                    tips.html(t).addClass("ui-state-error");
+                }
+                function removeTips(t) {
+                    tips.html(t).removeClass("ui-state-error");
+                }
+
                 $("#consultar").button({
                     icons: {primary: "ui-icon-search"}
-                }).click(function () {
+                });
 
-//                    $.getJSON("<?= $urlroot ?>/controler/consultacontroler.php", $("#form").serialize(), function (data) {
-//                        var items = [];
-//                        $.each(data, function (key, val) {
-//                            items.push("<li id='" + key + "'>" + val + "</li>");
-//                        });
-//                    });
+                $("#form").submit(function () {
+
+                    removeTips("");
 
                     $.ajax({
                         type: "POST",
@@ -32,8 +41,20 @@ include_once 'config.php';
                         dataType: "html",
                         data: $("#form").serialize(),
                         cache: false,
+                        beforeSend: function () {
+                            $("#consultar").button("disable");
+                        },
                         success: function (html) {
-                            montaTabela(html);
+
+                            if (html.substring(0, 1) === "{") {
+                                html = html.replace(/(\r\n|\n|\r)/gm,"<br/>");
+                                montaTabela($.parseJSON(html));
+                                $("#patrimonioid").select();
+                            } else {
+                                updateTips(html);
+                                $("#patrimonioid").select();
+                            }
+                            $("#consultar").button("enable");
                         }
                     });
 
@@ -42,16 +63,26 @@ include_once 'config.php';
 
                 function montaTabela(json) {
 
-                    var tabela = $("#tabela");
+                    var tabela = $("#tabela tbody");
                     tabela.html("");
                     var tr = "";
-                    for ($i = 0; $i < json.length; $i++) {
-                        tr += "<tr><td>" + key + ": </td><td>" + val + "</td></tr>";
-                    }
+                    $.each(json, function (key, val) {
+                        tr += "<tr><td width='9%'>" + key + ": </td><td>" + val + "</td></tr>";
+                    });
 
                     tabela.append(tr);
                 }
-                ;
+
+                $('.table').dataTable({
+                    "scrollX": "970px",
+                    "columnDefs": [
+                        {
+                            "searchable": false,
+                            "orderable": false,
+                            "targets": 0
+                        }
+                    ]
+                });
 
             });
         </script>
@@ -59,7 +90,7 @@ include_once 'config.php';
     </head>
     <body>
 
-        <div class="ui-widget-content" id="index-body" style="min-width: 200px; max-width: 600px; height: 70%; margin: 20px;">
+        <div class="ui-widget-content" id="index-body" style="min-width: 234px; max-width: 600px; margin: 20px;">
             <div class="linha-form">
                 <div class="ui-widget-header">Consultar Patrimônio</div>
                 <br/>
@@ -67,9 +98,13 @@ include_once 'config.php';
                     <div class="coluna-form">
                         <label>Número</label>
                         <input type="text" 
+                               id="patrimonioid"
                                name="patrimonioid"                            
                                class="text ui-widget-content ui-corner-all"
-                               style="width: 100px;">
+                               style="width: 100px;"
+                               maxlength="9"
+                               required
+                               autofocus>
                     </div>
                     <div class="coluna-form">
                         <br/>
@@ -80,20 +115,67 @@ include_once 'config.php';
                             data-titulo="Relatório de Requisição">
                             Consultar
                         </button>
+                        <input type="hidden" 
+                               name="control" 
+                               value="consulta">
                     </div>
-                    <input type="hidden" 
-                           name="control" 
-                           value="consulta">
+                    <div class="coluna-form msg" style="margin: 25px 0 5px 5px;">
+
+                    </div>
                 </form>
             </div>
             <div class="linha-form">
-                <div class="coluna-form">
-                    <table id="tabela">
+                <table id="tabela" class="table" cellspacing="0"  >
+                    <thead class="ui-widget-header"> 
                         <tr>
-                            <td>oi</td>
+                            <th colspan="2">
+                                <br/>
+                            </th>
                         </tr>
-                    </table>
-                </div>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td width="9%">Patrimônio: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Produto: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Local: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Departamento: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Data da Compra: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Data Implantação: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Fim da Garantia: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Estado de Conservação: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Fornecedor: </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td width="9%">Observação: </td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </body>
